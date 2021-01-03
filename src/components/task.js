68b3426d36aacaa5a1e05c260aa525095d347d91
Swetabh suman <swtbhsmn@gmail.com>
2021-01-03 16:22:25 +0530

    add mark button

--- src/components/task.js
+++ src/components/task.js
@@ -4,7 +4,7 @@ import AddIcon from '@material-ui/icons/Add';
 import TaskTable from "./task_data";
 import Modal from '@material-ui/core/Modal';
 import { withStyles } from '@material-ui/core/styles';
-import { Typography ,Link,Box} from "@material-ui/core";
+import { Typography, Link, Box } from "@material-ui/core";
 const useStyles = (theme) => ({
   paper: {
     position: 'absolute',
@@ -22,9 +22,10 @@ class Task extends React.Component {
       employee: "",
       employees: [],
       task_completed: [],
-      task_deleted:[],
+      task_deleted: [],
       open: false,
-      complete:0
+      complete: 0,
+      active: 0,
     }
 
     this.onChangeHandler.bind(this.onChangeHandler);
@@ -44,35 +45,36 @@ class Task extends React.Component {
   }
 
   onEmployeeAdd = () => {
-   
-    const { employee ,employees} = this.state;
-    if(employees.length===0){
+    let count = 0;
+    const { employee, employees } = this.state;
+    if (employees.length === 0) {
       this.setState({
-        task_deleted:[]
+        task_deleted: []
       })
     }
     if (employee !== '') {
+      count++;
       const userInput = {
 
-        employeeId: this.state.employees.length+1,
+        employeeId: this.state.employees.length + 1,
 
         employeeName: employee
       };
 
       const employees = [...this.state.employees];
       employees.push(userInput);
-
+      let active_task = this.state.active + count;
       this.setState({
         employee: "",
         employees,
-
+        active: active_task
       });
     }
 
   }
 
   onDeleteItem = (key) => {
-   
+    let count_delete_task = 0;
     let employees = [...this.state.employees];
     let deletedTask = [...this.state.task_deleted];
     let today = new Date();
@@ -84,18 +86,20 @@ class Task extends React.Component {
     let update_index = employees.findIndex(item => item.employeeId === key);
 
     let task_update = {
-      task_id:this.state.employees[update_index].employeeId,
-      task_name:this.state.employees[update_index].employeeName,
-      time_completed:time_format,
+      task_id: this.state.employees[update_index].employeeId,
+      task_name: this.state.employees[update_index].employeeName,
+      time_completed: time_format,
     }
 
     deletedTask.push(task_update);
 
     let updateList = employees.filter(item => item.employeeId !== key);
-
+    count_delete_task++;
+    let active_task = this.state.active - count_delete_task;
     this.setState({
       employees: updateList,
-      task_deleted: deletedTask
+      task_deleted: deletedTask,
+      active: active_task
     });
 
 
@@ -111,7 +115,7 @@ class Task extends React.Component {
     this.setState({ employees: employees })
   }
 
-  markCompleted=(x,key)=>{
+  markCompleted = (x, key) => {
     let employees = [...this.state.employees];
     let completedTask = [...this.state.task_completed];
     let today = new Date();
@@ -123,23 +127,23 @@ class Task extends React.Component {
     let update_index = employees.findIndex(item => item.employeeId === key);
 
     let task_update = {
-      task_id:this.state.employees[update_index].employeeId,
-      task_name:this.state.employees[update_index].employeeName,
-      time_completed:time_format,
+      task_id: this.state.employees[update_index].employeeId,
+      task_name: this.state.employees[update_index].employeeName,
+      time_completed: time_format,
     }
-   
+
     completedTask.push(task_update);
     console.log(completedTask);
 
-    let complete = this.state.complete+x;
+    let complete = this.state.complete + x;
 
     this.setState({
-      complete:complete,
-      task_completed:completedTask
-    },()=>{
-      if(this.state.employees.length===this.state.complete){
+      complete: complete,
+      task_completed: completedTask
+    }, () => {
+      if (this.state.employees.length === this.state.complete) {
         this.handleOpen()
-      
+
       }
     })
   }
@@ -149,53 +153,53 @@ class Task extends React.Component {
   };
 
   handleClose = () => {
-    this.setState({ open: false,task_completed:[],complete:0 ,employees:[]})
+    this.setState({ open: false, task_completed: [], complete: 0, employees: [], active: 0 })
   };
 
   render() {
     const { classes } = this.props;
 
-    const Footer=()=>{
-    
-        return (
-            <Typography variant="body2" color="textPrimary" align="center">
-                {'Copyright © '}
-                <Link color="primary" href="https://www.linkedin.com/in/swetabhsuman/">
-                    SwetabhSuman
+    const Footer = () => {
+
+      return (
+        <Typography variant="body2" color="textPrimary" align="center">
+          {'Copyright © '}
+          <Link color="primary" href="https://www.linkedin.com/in/swetabhsuman/">
+            SwetabhSuman
           </Link>{' '}
-                {new Date().getFullYear()}
-                {'.'}
-            </Typography>
-        );
-   
+          {new Date().getFullYear()}
+          {'.'}
+        </Typography>
+      );
+
     }
     const body = (
       <div className={classes.paper}>
         <h2 id="simple-modal-title" style={{ color: "#32a848" }}>Successfully Task Completed!</h2>
 
-      <table>
-        <tbody>
-          <tr>
-          <th style={{padding:10}}>Task Id</th>
-            <th style={{padding:10}}>Task Name</th>
-            <th style={{padding:10}}>Task End Time</th>
-            <th style={{padding:10}}>Task Completed</th>
-          </tr>
-          {this.state.task_completed.map((item, key) => {
-          return (
-            <tr key={key} >
-              <td className="text-center"> {item.task_id}</td>
-              <td className="text-center"> {item.task_name}</td>
-              <td className="text-center"> {item.time_completed}</td>
-              <td className="text-center"> <i className="material-icons" style={{ color: "#32a848" }}>done</i></td>
-               
+        <table>
+          <tbody>
+            <tr>
+              <th style={{ padding: 10 }}>Task Id</th>
+              <th style={{ padding: 10 }}>Task Name</th>
+              <th style={{ padding: 10 }}>Task End Time</th>
+              <th style={{ padding: 10 }}>Task Completed</th>
             </tr>
-          );
-        })}
-        </tbody>
-      </table>
+            {this.state.task_completed.map((item, key) => {
+              return (
+                <tr key={key} >
+                  <td className="text-center"> {item.task_id}</td>
+                  <td className="text-center"> {item.task_name}</td>
+                  <td className="text-center"> {item.time_completed}</td>
+                  <td className="text-center"> <i className="material-icons" style={{ color: "#32a848" }}>done</i></td>
 
-       
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+
+
 
       </div>
     );
@@ -213,7 +217,7 @@ class Task extends React.Component {
         <div className="taskDetails">
           <div className="active">
             <p>Active Task</p>
-            <p>{this.state.employees.length-this.state.complete}</p>
+            <p>{this.state.active - this.state.complete}</p>
           </div>
           <div className="completed"><p>Completed Task</p>
             <p>  {this.state.complete}</p>
@@ -234,14 +238,14 @@ class Task extends React.Component {
         <div className="table-container">
           <Container maxWidth="md" >
 
-            < TaskTable employees_list={this.state.employees} click={this.onDeleteItem} onEdit={this.onEditItem} clickMark={this.markCompleted}/>
+            < TaskTable employees_list={this.state.employees} click={this.onDeleteItem} onEdit={this.onEditItem} clickMark={this.markCompleted} />
 
 
           </Container>
         </div>
-      
+
         <Box mt={5} className="footer">
-                        <Footer />
+          <Footer />
         </Box>
       </div>
     );
